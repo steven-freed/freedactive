38692d7d59d4da07362adf261cda1b23b40c9187
Steven Freed <saf236@scarletmail.rutgers.edu>
2020-02-08 21:05:33 -0500

    [Version 1.1.3] added fa-ui component lib

--- makeugly.js
+++ makeugly.js
@@ -1,14 +1,25 @@
 var fs = require('fs');
 var uglify = require('uglify-js');
+var uglifycss = require('uglifycss');
 
 var code = {
-    './lib/freedactive.js': null
+    './lib/freedactive.js': null,
+    './lib/fa-ui.js': null,
+    './lib/fa-ui.css': null
 };
 
-Object.keys(code).map(function(file) {
-    var content = fs.readFileSync(file, 'utf8');
-    code[file] = content.slice(0, content.indexOf('global.'));
-});
+for (key in code) {
+    var fileType = key.slice(key.lastIndexOf('.') + 1);
+    var content = fs.readFileSync(key, 'utf8');
+    code[key] = content.slice(0, content.indexOf('global.'));
+    var newFile = './dist' + key.slice(key.lastIndexOf('/'), key.lastIndexOf('.')) +
+    '.min' + key.slice(key.lastIndexOf('.'));
 
-var minicode = uglify.minify(code);
-fs.writeFileSync('./dist/freedactive.min.js', minicode.code);
+    var minicode;
+    if (fileType === 'js') {
+        minicode = uglify.minify(code[key]);
+    } else if (fileType === 'css') {
+        minicode = uglifycss.processFiles([key]);
+    }
+    fs.writeFileSync(newFile, minicode.code ? minicode.code : minicode);
+}
